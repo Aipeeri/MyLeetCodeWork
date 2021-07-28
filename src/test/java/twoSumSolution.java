1c68d791941ca49d913b43597c7fac06096cb126
Aipeeri <amisiralieva@gmail.com>
2021-07-27 23:34:59 -0500

    Leetcode Task

--- src/test/java/twoSumSolution.java
+++ src/test/java/twoSumSolution.java
@@ -13,7 +13,7 @@ public void runTwoSum(){
         public int[] twoSum(int[] nums, int target){
 
             int[] arrInt= new int[3];
-            outer:
+
             for(int i=0; i<=nums.length-1; i++){
                 for(int j=i+1; j<=nums.length-1; j++){
                     if(nums[i]+nums[j]==target){
